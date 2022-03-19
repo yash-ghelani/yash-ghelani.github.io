f22ee849aaede44bb854d8a41020dc4fb00c004a
unknown <yashghelani08@gmail.com>
2022-03-19 20:06:49 +0000

    random character selection

--- script.js
+++ script.js
@@ -7,4 +7,11 @@ function flipImage(image) {
     else {
         Image_Id.src = "images/"+id+".jpg";
     }
-}     
+} 
+
+function chooseChar(){
+    var files = ['al', 'amy', 'ben', 'carmen', 'daniel', 'david', 'emma', 'eric', 'farah', 'gabe', 'joe', 'jordan', 'katie', 'laura', 'leo', 'lily', 'liz', 'mia', 'mike', 'nick', 'olivia', 'rachel', 'sam', 'sofia'];
+    var image = files[Math.floor(Math.random()*files.length)];
+    var character = document.getElementById('character');
+    character.src = "images/"+image+".jpg";
+}
