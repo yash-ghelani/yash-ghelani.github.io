02bdf5001772d3c7324cf3ab84cd015a63270102
Yash Ghelani <yashghelani08@gmail.com>
2024-07-31 00:05:05 +0100

    homepage

---
+++ src/app/ui/fonts.js
@@ -0,0 +1,3 @@
+import { Outfit } from 'next/font/google';
+
+export const outift = Outfit({ subsets: ['latin'] });
---
+++ src/components/hero-section.jsx
@@ -0,0 +1,12 @@
+import { outift } from '@/app/ui/fonts';
+import React from 'react';
+
+const HeroSection = () => {
+	return (
+		<section>
+			<h1 className={`${outift.className} antialiased text-white text-6xl text-opacity-75 font-bold`}>Yash Ghelani</h1>
+		</section>
+	);
+};
+
+export default HeroSection;
