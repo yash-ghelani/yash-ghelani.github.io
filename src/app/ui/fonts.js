7ea1f701980bb4835e7ec2ca380ff9965496cc67
Yash Ghelani <yashghelani08@gmail.com>
2024-07-31 00:29:50 +0100

    homepage content (no stlying)

--- src/app/ui/fonts.js
+++ src/app/ui/fonts.js
@@ -1,3 +1,4 @@
-import { Outfit } from 'next/font/google';
+import { Outfit, Fira_Code } from 'next/font/google';
 
 export const outift = Outfit({ subsets: ['latin'] });
+export const fira = Fira_Code({ subsets: ['latin'] });
