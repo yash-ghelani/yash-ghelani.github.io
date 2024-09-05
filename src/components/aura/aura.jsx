468a01627b803ab7a9483faa351d274e4d00f352
Yash Ghelani <yashghelani08@gmail.com>
2024-09-06 00:44:34 +0100

    aura improvements and mouse effect

--- src/components/aura/aura.jsx
+++ src/components/aura/aura.jsx
@@ -1,30 +1,59 @@
-import React from 'react';
+'use client'; // Ensure this is a client component
+
+import React, { useState, useEffect } from 'react';
 
 export const COLORS = {
-    Pink: 'bg-pink-500',
-    Blue: 'bg-blue-500',
-    Orange: 'bg-orange-500',
-    Purple: 'bg-purple-500',
-    Cyan: 'bg-sky-500',
-    Red: 'bg-red-500',
+	Pink: 'bg-pink-500',
+	Blue: 'bg-blue-500',
+	Orange: 'bg-orange-500',
+	Purple: 'bg-purple-500',
+	Cyan: 'bg-sky-500',
+	Red: 'bg-red-500',
 };
 
 export const POSITIONS = {
-    TopLeft: '-left-1/4 top-1/3',
-    MidLeft: '-left-1/4 top-1/2 transform -translate-y-1/2',
-    BottomLeft: '-left-1/4 bottom-1/3',
-    TopRight: '-right-1/4 top-1/3',
-    MidRight: '-right-1/4 top-1/2 transform -translate-y-1/2',
-    BottomRight: '-right-1/4 bottom-1/3',
+	TopLeft: '-left-1/4 top-1/3',
+	MidLeft: '-left-1/4 top-1/2 transform -translate-y-1/2',
+	BottomLeft: '-left-1/4 bottom-1/3',
+	TopRight: '-right-1/4 top-1/3',
+	MidRight: '-right-1/4 top-1/2 transform -translate-y-1/2',
+	BottomRight: '-right-1/4 bottom-1/3',
 };
 
-const Aura = ({
-	color = COLORS.Pink,
-	position = POSITIONS.TopLeft,
-}) => {
+const Aura = ({ color = COLORS.Pink, position = POSITIONS.TopLeft }) => {
+	const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+	useEffect(() => {
+		const handleMouseMove = (e) => {
+			const { innerWidth, innerHeight } = window;
+
+			// Mouse-based movement offset
+			const mouseX = (e.clientX / innerWidth) * 250 - 125; // Centering at 0
+			const mouseY = (e.clientY / innerHeight) * 250 - 125; // Centering at 0
+
+			// Set mouse movement offset
+			setOffset({
+				x: mouseX,
+				y: mouseY,
+			});
+		};
+
+		// Add mousemove listener
+		window.addEventListener('mousemove', handleMouseMove);
+
+		// Cleanup function
+		return () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+		};
+	}, []);
+
 	return (
 		<div
-			className={`absolute ${position} w-[60vw] h-[60vw] ${color} opacity-40 rounded-full filter blur-3xl z-[0]`}
+			className={`absolute ${position} w-[60vw] h-[60vw] ${color} opacity-60 rounded-full filter blur-[300px] z-[-1]`}
+			style={{
+				transform: `translate(${offset.x}px, ${offset.y}px)`,
+				transition: 'transform 0.65s ease-out', // Smooth movement for mouse following
+			}}
 		/>
 	);
 };
