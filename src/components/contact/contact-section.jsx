62953ff54f88587d0975226c0f32e970d17bb982
Yash Ghelani <yashghelani08@gmail.com>
2024-09-13 00:05:22 +0100

    make contact page responsive

--- src/components/contact/contact-section.jsx
+++ src/components/contact/contact-section.jsx
@@ -36,73 +36,84 @@ const ContactSection = () => {
 	};
 
 	return (
-		<section className='flex flex-col h-svh m-auto max-w-[1280px] justify-center xl:items-start items-center gap-10'>
-			<h2 className='font-semibold tracking-widest text-5xl opacity-55'>
-				get in touch
-			</h2>
+		<section className='min-h-svh xl:max-w-[1280px] max-w-[600px] w-[80vw] m-auto flex flex-col justify-center'>
+			<div className='flex flex-col gap-10 m-auto w-full'>
+				<h2 className='font-semibold tracking-widest text-clamp-xl opacity-55'>
+					get in touch
+				</h2>
 
-			<div className='flex flex-col xl:flex-row justify-between xl:w-full w-1/2'>
-				<form onSubmit={handleSubmit} className='w-full xl:w-1/2'>
-					<div className='flex flex-col gap-2 mb-4'>
-						<label htmlFor='name' className='block font-extralight opacity-55'>
-							your name
-						</label>
-						<input
-							type='text'
-							id='name'
-							name='name'
-							value={formData.name}
-							onChange={handleChange}
-							required
-							className='w-full bg-white/5 rounded-2xl p-3 backdrop-blur-3xl border border-white/10 shadow-md'
-						/>
-					</div>
+				<div className='flex flex-col xl:flex-row justify-between gap-20'>
+					<form onSubmit={handleSubmit} className='w-full xl:w-1/2'>
+						<div className='flex flex-col gap-2 mb-4'>
+							<label
+								htmlFor='name'
+								className='block font-extralight opacity-55'
+							>
+								your name
+							</label>
+							<input
+								type='text'
+								id='name'
+								name='name'
+								value={formData.name}
+								onChange={handleChange}
+								required
+								className='w-full bg-white/5 rounded-2xl p-3 backdrop-blur-3xl border border-white/10 shadow-md'
+							/>
+						</div>
 
-					<div className='flex flex-col gap-2 mb-4'>
-						<label htmlFor='email' className='block font-extralight opacity-55'>
-							your email
-						</label>
-						<input
-							type='email'
-							id='email'
-							name='email'
-							value={formData.email}
-							onChange={handleChange}
-							required
-							className='w-full bg-white/5 rounded-2xl p-3 backdrop-blur-3xl border border-white/10 shadow-md'
-						/>
-					</div>
+						<div className='flex flex-col gap-2 mb-4'>
+							<label
+								htmlFor='email'
+								className='block font-extralight opacity-55'
+							>
+								your email
+							</label>
+							<input
+								type='email'
+								id='email'
+								name='email'
+								value={formData.email}
+								onChange={handleChange}
+								required
+								className='w-full bg-white/5 rounded-2xl p-3 backdrop-blur-3xl border border-white/10 shadow-md'
+							/>
+						</div>
 
-					<div className='flex flex-col gap-2 mb-4'>
-						<label htmlFor='message' className='block font-extralight opacity-55'>
-							your message
-						</label>
-						<textarea
-							id='message'
-							name='message'
-							value={formData.message}
-							onChange={handleChange}
-							required
-							className='w-full bg-white/5 rounded-2xl p-3 backdrop-blur-3xl border border-white/10 shadow-md'
-						/>
-					</div>
+						<div className='flex flex-col gap-2 mb-4'>
+							<label
+								htmlFor='message'
+								className='block font-extralight opacity-55'
+							>
+								your message
+							</label>
+							<textarea
+								id='message'
+								name='message'
+								value={formData.message}
+								onChange={handleChange}
+								required
+								className='w-full bg-white/5 rounded-2xl p-3 backdrop-blur-3xl border border-white/10 shadow-md'
+							/>
+						</div>
 
-					<button
-						type='submit'
-						className='bg-white/5 rounded-full backdrop-blur-3xl border border-white/10 shadow-md px-10 py-2 my-10'
-					>
-						<span className='font-semibold tracking-widest opacity-55'>
-							send
-						</span>
-					</button>
-				</form>
-				<Image
-					src={'/social/paper-plane.png'}
-					width={200}
-					height={200}
-					alt='Contact icon - paper plane'
-					className='m-auto'
-				/>
+						<button
+							type='submit'
+							className='bg-white/5 rounded-full backdrop-blur-3xl border border-white/10 shadow-md px-10 py-2 my-10'
+						>
+							<span className='font-semibold tracking-widest opacity-55'>
+								send
+							</span>
+						</button>
+					</form>
+					<Image
+						src={'/social/paper-plane.png'}
+						width={200}
+						height={200}
+						alt='Contact icon - paper plane'
+						className='m-auto'
+					/>
+				</div>
 			</div>
 		</section>
 	);
