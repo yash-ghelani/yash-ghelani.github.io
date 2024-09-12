89377ee8496fd9e1e403a28d934f35f31833a8d3
Yash Ghelani <yashghelani08@gmail.com>
2024-09-13 00:57:13 +0100

    add favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,20 @@
-import "./globals.css";
-import { outift } from "./ui/fonts";
-
+import './globals.css';
+import { outift } from './ui/fonts';
 
 export const metadata = {
-  title: "Yash's Portfolio",
-  description: "Personal website - built and designed by Yash Ghelani",
+	title: "Yash's Portfolio",
+	description: 'Personal website - built and designed by Yash Ghelani',
+	icons: {
+		icon: '/favicons/favicon.ico', // Path to favicon
+		apple: '/favicons/apple-touch-icon.png', // Apple touch icon
+		shortcut: '/favicons/favicon-32x32.png', // Shortcut icon
+	},
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={outift.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={outift.className}>{children}</body>
+		</html>
+	);
 }
