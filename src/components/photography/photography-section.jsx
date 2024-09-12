6c2db753ee278fc43a2f6c24507d939a4e706dda
Yash Ghelani <yashghelani08@gmail.com>
2024-09-13 00:05:08 +0100

    photography section refactor

--- src/components/photography/photography-section.jsx
+++ src/components/photography/photography-section.jsx
@@ -10,10 +10,55 @@ import 'swiper/css/pagination';
 
 // import required modules
 import { Autoplay, EffectCoverflow } from 'swiper/modules';
+import Image from 'next/image';
 
 const PhotographySection = () => {
+	
+	const polaroids = [
+		'/polaroids/1.png',
+		'/polaroids/2.png',
+		'/polaroids/3.png',
+		'/polaroids/4.png',
+		'/polaroids/5.png',
+		'/polaroids/6.png',
+		'/polaroids/7.png',
+		'/polaroids/8.png',
+		'/polaroids/9.png',
+		'/polaroids/10.png',
+		'/polaroids/11.png',
+		'/polaroids/12.png',
+		'/polaroids/13.png',
+		'/polaroids/14.png',
+		'/polaroids/15.png',
+		'/polaroids/16.png',
+		'/polaroids/17.png',
+		'/polaroids/18.png',
+		'/polaroids/19.png',
+		'/polaroids/20.png',
+		'/polaroids/21.png',
+		'/polaroids/22.png',
+		'/polaroids/23.png',
+		'/polaroids/24.png',
+		'/polaroids/25.png',
+		'/polaroids/26.png',
+		'/polaroids/27.png',
+		'/polaroids/28.png',
+		'/polaroids/29.png',
+		'/polaroids/30.png',
+		'/polaroids/31.png',
+		'/polaroids/32.png',
+		'/polaroids/33.png',
+		'/polaroids/34.png',
+		'/polaroids/35.png',
+		'/polaroids/36.png',
+		'/polaroids/37.png',
+		'/polaroids/38.png',
+		'/polaroids/39.png',
+		'/polaroids/40.png',
+	];
+
 	return (
-		<section className='min-h-svh max-w-[1280px] w-[80vw] m-auto flex flex-col justify-center border'>
+		<section className='min-h-svh xl:max-w-[1280px] max-w-[600px] w-[80vw] m-auto flex flex-col justify-center'>
 			<div className='flex flex-col w-full gap-20 m-auto'>
 				<h2 className='font-semibold tracking-widest text-clamp-xl opacity-55'>
 					photography
@@ -38,126 +83,19 @@ const PhotographySection = () => {
 					modules={[Autoplay, EffectCoverflow]}
 					className='mySwiper w-full'
 				>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/1.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/2.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/3.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/4.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/5.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/6.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/7.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/8.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/9.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/10.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/11.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/12.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/13.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/14.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/15.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/16.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/17.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/18.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/19.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/20.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/21.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/22.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/23.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/24.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/25.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/26.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/27.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/28.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/29.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/30.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/31.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/32.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/33.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/34.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/35.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/36.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/37.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/38.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/39.png' />
-					</SwiperSlide>
-					<SwiperSlide className='!w-[300px] !h-[300px]'>
-						<img className='block' src='/polaroids/40.png' />
-					</SwiperSlide>
+					{polaroids.map((src, index) => (
+						<SwiperSlide
+							className='max-w-[300px]'
+							key={index}
+						>
+							<Image
+								src={src}
+								alt={'polaroid ' + index}
+								width={300}
+								height={300}
+							/>
+						</SwiperSlide>
+					))}
 				</Swiper>
 			</div>
 		</section>
