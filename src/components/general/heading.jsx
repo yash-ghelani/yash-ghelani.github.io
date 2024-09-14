c32440771afa31fedc35fdd99338f8e7c13c73ac
Yash Ghelani <yashghelani08@gmail.com>
2024-09-14 13:49:01 +0100

    heading animation changes

--- src/components/general/heading.jsx
+++ src/components/general/heading.jsx
@@ -4,14 +4,14 @@ import React from 'react';
 import { useIntersectionObserver } from '@uidotdev/usehooks';
 
 const Heading = ({ children }) => {
-	const [ref, entry] = useIntersectionObserver({ threshold: 0.1 });
-	const isVisible = entry?.isIntersecting;
+	const [ref, entry] = useIntersectionObserver({ threshold: 0.5 });
 
 	return (
 		<h2
 			ref={ref}
+			style={{ willChange: 'opacity, transform' }}
 			className={`font-semibold tracking-widest text-clamp-xl transition-all duration-[1.5s] delay-300 ease-out ${
-				isVisible
+				entry?.isIntersecting
 					? 'opacity-65 translate-y-0'
 					: 'opacity-0 translate-y-10'
 			}`}
