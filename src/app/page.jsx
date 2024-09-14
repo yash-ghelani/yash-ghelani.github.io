6f56f83d7b06a49e9005bffc280fe8b555cd4b41
Yash Ghelani <yashghelani08@gmail.com>
2024-09-14 02:02:56 +0100

    reduce load time

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,7 +17,7 @@ export default function Home() {
 		// Simulate a loading delay of 5 seconds
 		const timeout = setTimeout(() => {
 			setIsLoading(false);
-		}, 5000); // 5 seconds delay
+		}, 3000);
 
 		// Cleanup the timeout when the component unmounts
 		return () => clearTimeout(timeout);
